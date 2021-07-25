673a6611a4c31ced7b96bf2fc2d78840efde0ab3
gonzalo10 <gonzalo.achiaga@gmail.com>
2021-07-25 19:44:32 +0200

    feat: added testing

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,23 +1,25 @@
 require('@nomiclabs/hardhat-waffle')
-const fs = require('fs')
-// const privateKey = fs.readFileSync(".secret").toString().trim() || "01234567890123456789";
-// const infuraId = fs.readFileSync(".infuraid").toString().trim() || "";
+require('dotenv').config()
 
+console.log(process.env.PRIVATE_KEY)
 module.exports = {
 	defaultNetwork: 'hardhat',
 	networks: {
+		localhost: {
+			url: 'http://localhost:8545'
+			/*
+        notice no mnemonic here? it will just use account 0 of the hardhat node to deploy
+        (you can put in a mnemonic here to set the deployer locally)
+      */
+		},
 		hardhat: {
 			chainId: 1337
 		},
 		mumbai: {
-			// Infura
-			// url: `https://polygon-mumbai.infura.io/v3/${process.env.INFURA_ID}`
 			url: 'https://rpc-mumbai.matic.today',
 			accounts: [process.env.PRIVATE_KEY]
 		},
 		matic: {
-			// Infura
-			// url: `https://polygon-mainnet.infura.io/v3/${process.env.INFURA_ID}`,
 			url: 'https://rpc-mainnet.maticvigil.com',
 			accounts: [process.env.PRIVATE_KEY]
 		}
